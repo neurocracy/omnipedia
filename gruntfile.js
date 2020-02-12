dc7b0e1b9a9dc0840bef42448899681a4f7ef61d
Matei Stanca <i@ambientimpact.com>
2020-02-12 15:21:42 -0500

    Added root Grunt tasks to delegate to modules and themes.

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  const gruntTargets = {
+    modules:  'drupal/modules/omnipedia',
+    themes:   'drupal/themes/omnipedia'
+  };
+
+  var config = {shell: {}};
+
+  for (const targetName in gruntTargets) {
+    if (!gruntTargets.hasOwnProperty(targetName)) {
+      continue;
+    }
+
+    // Run the given Grunt command on the specified target.
+    //
+    // @see https://github.com/sindresorhus/grunt-shell
+    config.shell[targetName] = {
+      command: gruntCommand => `grunt ${gruntCommand}`,
+      options: {
+        execOptions: {
+          cwd: gruntTargets[targetName]
+        }
+      }
+    };
+
+    // Register each target as a task that can be run with a command. I.e.:
+    //
+    // grunt target:command
+    //
+    // @see https://gruntjs.com/api/grunt.task
+    grunt.registerTask(targetName, function(gruntCommand) {
+      grunt.task.run('shell:' + targetName + ':' + gruntCommand);
+    });
+  }
+
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.initConfig(config);
+};
