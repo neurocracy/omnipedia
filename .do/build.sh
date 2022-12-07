631406229a127aac041b04d2f0304714f3fde86c
Matei Stanca <i@ambientimpact.com>
2022-12-06 20:54:54 -0500

    Trying out Bash scripts to version control DigitalOcean build/run.

---
+++ .do/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yarn add \
+  "drupal-omnipedia-block@workspace:^4" \
+  "drupal-omnipedia-changes@workspace:^4" \
+  "drupal-omnipedia-content@workspace:^4" \
+  "drupal-omnipedia-media@workspace:^4" \
+  "drupal-omnipedia-site-theme@workspace:^4"
+
+yarn build:production
---
+++ .do/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+heroku-php-apache2 -F .php/php-fpm.inc.conf drupal
