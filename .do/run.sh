58a00664805e5389db53a3f7c50fc328b226150f
Matei Stanca <i@ambientimpact.com>
2022-12-08 03:03:18 -0500

    .do/run.sh: Add temporary "drush rebuilder asset" workaround + docs.

--- .do/run.sh
+++ .do/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Rebuild asset libraries as a temporary workaround for local aggregation.
+#
+# Since App Platform wipes the filesystem on every deploy, locally saved
+# aggregated assets get wiped as well, and neither Drupal nor AdvAgg clue in
+# that the files they'd previously aggregated no longer exist, which results in
+# a 404. This is a temporary workaround to invalidate all the aggregated assets
+# and cause them to be rebuilt so they exist in the new filesystem after a
+# deploy.
+#
+# @todo Implement backing up aggregated assets to DigitalOcean Spaces when they
+#   get created and attempt to pull them in after a deploy, only rebuilding
+#   assets if any of them are not found.
+drush rebuilder asset
+
 # Run the Heroku PHP buildpack PHP-FPM with our custom config appended.
 #
 # @see https://devcenter.heroku.com/articles/custom-php-settings
