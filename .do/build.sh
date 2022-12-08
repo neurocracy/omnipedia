4e36701adba3644d4d546d6cd3136ce03f87e33c
Matei Stanca <i@ambientimpact.com>
2022-12-08 02:11:25 -0500

    Added documentation to .do/build.sh and .do/run.sh

--- .do/build.sh
+++ .do/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Because it's not yet possible to customize the order that autodetected
+# buildpacks run their builds on DigitalOcean App Platform, and because the PHP
+# buildpack always runs after the Node.js buildpack, we have to add these
+# workspace items after the fact because they're only present after Composer has
+# pulled them in and would otherwise stop the build with an error when Yarn
+# would try to find them.
 yarn add \
   "drupal-omnipedia-block@workspace:^4" \
   "drupal-omnipedia-changes@workspace:^4" \
