1c9e7def90bb1ef2cfbbe3a19b324c8c1b094afd
Matei Stanca <i@ambientimpact.com>
2023-01-08 11:45:39 -0500

    settings.php: Cleaned up and expanded Paranoia config split docblock.

--- drupal/sites/default/settings.php
+++ drupal/sites/default/settings.php
@@ -101,10 +101,11 @@ foreach ($databases as $databaseKey => &$database) {
  */
 $settings['config_sync_directory'] = $app_root . '/../drupal_config/sync';
 
-// Enable the 'dev' configuration split in this environment.
-
 /**
  * Enable the Paranoia configuration split by default.
+ *
+ * @see https://www.drupal.org/project/config_split/issues/3109103
+ *   Configuration Split issue explaining why a separate split is necessary.
  */
 $config['config_split.config_split.paranoia']['status'] = true;
 
