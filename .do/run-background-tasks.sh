50a6be4289f0a4dc019e8caf9f33792f94f00c8f
Matei Stanca <i@ambientimpact.com>
2023-02-02 18:29:35 -0500

    .do/run-background-tasks.sh: Added 5 second initial delay to running.

--- .do/run-background-tasks.sh
+++ .do/run-background-tasks.sh
@@ -7,6 +7,12 @@
 # @todo Implement some way to run cron on a more predictable schedule, possibly
 #   via parallel execution?
 
+# This adds an initial delay before doing anything (e.g. rebuilding assets).
+#
+# This is to give the web component a little bit of lead time when deploying.
+echo "Sleeping for 5 seconds to allow the web component to start."
+sleep 5;
+
 # Rebuild asset libraries as a temporary workaround for local aggregation.
 #
 # Since App Platform wipes the filesystem on every deploy, locally saved
