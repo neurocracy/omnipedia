ecd35c5362c6a9c7c8e6fa496abdd26a323a0079
Matei Stanca <i@ambientimpact.com>
2023-06-16 15:48:59 -0400

    .do/build-web.sh: Updated workspace versions and added new for Drupal 10.

--- .do/build-web.sh
+++ .do/build-web.sh
@@ -14,11 +14,15 @@ composer dump-autoload --optimize --apcu
 # pulled them in and would otherwise stop the build with an error when Yarn
 # would try to find them.
 yarn add \
-  "drupal-omnipedia-block@workspace:^4" \
-  "drupal-omnipedia-changes@workspace:^5" \
-  "drupal-omnipedia-content@workspace:^4" \
-  "drupal-omnipedia-media@workspace:^4" \
-  "drupal-omnipedia-site-theme@workspace:^4"
+  "drupal-ambientimpact-core@workspace:^2" \
+  "drupal-ambientimpact-icon@workspace:^1" \
+  "drupal-ambientimpact-media@workspace:^2" \
+  "drupal-ambientimpact-ux@workspace:^1" \
+  "drupal-omnipedia-block@workspace:^5" \
+  "drupal-omnipedia-changes@workspace:^7" \
+  "drupal-omnipedia-content@workspace:^6" \
+  "drupal-omnipedia-media@workspace:^6" \
+  "drupal-omnipedia-site-theme@workspace:^6"
 
 yarn build:deploy
 
