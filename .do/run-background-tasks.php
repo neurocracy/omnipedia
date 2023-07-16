38a3b6b914fe0f65c477f85f73317d4bbd6dcdc4
Matei Stanca <i@ambientimpact.com>
2023-07-16 18:15:53 -0400

    Issue #3: Queues are now run sequentially and avoid overlapping runs.

--- .do/run-background-tasks.php
+++ .do/run-background-tasks.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Omnipedia\BackgroundTasks\WorkerProcess;
 use React\EventLoop\Loop;
+use React\Promise\ExtendedPromiseInterface;
+use function React\Async\series;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -20,25 +22,39 @@ $loop->addPeriodicTimer(900, function(): void {
   (new WorkerProcess('drush cron'))->start();
 });
 
-// Every 5 minutes, process jobs from the image style warmer.
-$loop->addPeriodicTimer(300, function(): void {
-  (new WorkerProcess(
-    'drush queue:process image_style_warmer_pregenerator'
-  ))->start();
-});
+// Every 10 minutes, run queues.
+$loop->addPeriodicTimer(600, function(): void {
 
-// Every 10 minutes, enqueue and process the warmer queue.
-$loop->addPeriodicTimer(600, function() use ($loop): void {
+  /** @var boolean Static flag indicating whether a run is currently in progress. */
+  static $running = false;
 
-  $loop->futureTick(function(): void {
-    (new WorkerProcess('drush warmer:enqueue ' . \implode(',', [
-      'omnipedia_wiki_node_changes',
-      'omnipedia_wiki_node_cdn',
-    ])))->start();
-  });
+  if ($running === true) {
+    return;
+  }
 
-  $loop->futureTick(function(): void {
-    (new WorkerProcess('drush queue:process warmer'))->start();
+  $running = true;
+
+  series([
+    function(): ExtendedPromiseInterface {
+      return (new WorkerProcess('drush warmer:enqueue ' . \implode(',', [
+        'omnipedia_wiki_node_changes',
+        'omnipedia_wiki_node_cdn',
+      ])))->start()->promise();
+    },
+    function(): ExtendedPromiseInterface {
+      return (new WorkerProcess(
+        'drush queue:process image_style_warmer_pregenerator'
+      ))->start()->promise();
+    },
+    function(): ExtendedPromiseInterface {
+      return (new WorkerProcess(
+        'drush queue:process warmer'
+      ))->start()->promise();
+    },
+  ])->always(function() use (&$running) {
+
+    $running = false;
+
   });
 
 });
