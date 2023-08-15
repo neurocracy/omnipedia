7b7f76bbf89221b41b9b0b6bad3678e37594e081
Matei Stanca <i@ambientimpact.com>
2023-08-15 11:42:24 -0400

    Re-added .ht.router.php scaffold file for GitHub Actions.

--- .do/build-web.sh
+++ .do/build-web.sh
@@ -2,6 +2,18 @@
 
 # Build the web service.
 
+# We need to remove this since it's not used in production but it is needed for
+# running PHPUnit functional tests under GitHub Actions. Trying to conditionally
+# remove it via
+# 'composer config --unset extra.drupal-scaffold.file-mapping."[web-root]/.htaccess"'
+# or multiple such variations with various characters in the last key escaped
+# seem to have no effect, despite the fact that fetching the config by leaving
+# out '--unset' works correctly.
+#
+# @todo Figure out why running the above command doesn't remove the key, add it
+#   back the scaffold, and remove this command.
+rm /workspace/web/.ht.router.php
+
 # Dump optimized Composer autoloader using APCu for production.
 #
 # @see https://getcomposer.org/doc/articles/autoloader-optimization.md
