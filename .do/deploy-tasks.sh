a2243f8580fb1d9acab2d5d3404d48f02b76d2b2
Matei Stanca <i@ambientimpact.com>
2023-08-26 13:24:57 -0400

    Added Hux patch to invalidate hook discovery w/ deployment identifier:
    https://www.drupal.org/project/hux/issues/3302312#comment-15207542
    
    Also removed the Hux discovery cache delete from .do/deploy-tasks.sh

--- .do/deploy-tasks.sh
+++ .do/deploy-tasks.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Delete the Hux discovery cache entry in case of hook changes.
-#
-# @see https://www.drupal.org/project/hux/issues/3302312#comment-15202523
-drush php:eval "\Drupal::service('cache.bootstrap')->delete('hux.discovery');"
-
 # Run any necessary database updates.
 drush -y updb
