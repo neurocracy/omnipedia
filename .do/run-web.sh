73309687cf471ced96068b06420618e8dde395a4
Matei Stanca <i@ambientimpact.com>
2023-10-02 16:36:42 -0400

    .do/run-web.sh: Remove hard coded web root; get from Drush.

--- .do/run-web.sh
+++ .do/run-web.sh
@@ -11,4 +11,7 @@ source "${BASH_SOURCE%/*}/run-common.sh"
 # @see https://github.com/heroku/heroku-buildpack-php/blob/main/bin/heroku-php-apache2
 #   Commandline usage detailed in the print_help() function, including how to
 #   pass this to the run command.
-heroku-php-apache2 -C .apache/httpd.inc.conf -F .php/php-fpm.inc.conf web
+heroku-php-apache2 -C .apache/httpd.inc.conf -F .php/php-fpm.inc.conf \
+  # This automagically gets the web root from Drush so we don't have to hard
+  # code it.
+  $(drush drupal:directory root)
