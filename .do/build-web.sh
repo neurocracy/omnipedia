6ac077a1e1c9558f33af455865c01a9eed473a77
Matei Stanca <i@ambientimpact.com>
2023-12-16 10:31:14 -0500

    .do/build-web.sh: grammar fix .ht.router.php comments + more generic.

--- .do/build-web.sh
+++ .do/build-web.sh
@@ -3,15 +3,14 @@
 # Build the web service.
 
 # We need to remove this since it's not used in production but it is needed for
-# running PHPUnit functional tests under GitHub Actions. Trying to conditionally
-# remove it via
+# running PHPUnit functional tests in CI. Trying to conditionally remove it via
 # 'composer config --unset extra.drupal-scaffold.file-mapping."[web-root]/.htaccess"'
 # or multiple such variations with various characters in the last key escaped
 # seem to have no effect, despite the fact that fetching the config by leaving
 # out '--unset' works correctly.
 #
 # @todo Figure out why running the above command doesn't remove the key, add it
-#   back the scaffold, and remove this command.
+#   back to the scaffold, and remove this command.
 rm /workspace/web/.ht.router.php
 
 # Dump optimized Composer autoloader using APCu for production.
