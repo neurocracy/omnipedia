2ec4c3296633a5ac18bc69800ac447b6d625543d
Matei Stanca <i@ambientimpact.com>
2024-01-13 10:51:12 -0500

    Moved secure-filesystem.sh to .do/build/

--- .do/build-background-tasks.sh
+++ .do/build-background-tasks.sh
@@ -5,4 +5,4 @@ source "${BASH_SOURCE%/*}/build/composer.sh"
 # Secure the file system by removing write permissions.
 #
 # @see https://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script#6659698
-source "${BASH_SOURCE%/*}/secure-filesystem.sh"
+source "${BASH_SOURCE%/*}/build/secure-filesystem.sh"
--- .do/build-web.sh
+++ .do/build-web.sh
@@ -7,4 +7,4 @@ source "${BASH_SOURCE%/*}/build/composer.sh"
 source "${BASH_SOURCE%/*}/build/yarn.sh"
 
 # @see https://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script#6659698
-source "${BASH_SOURCE%/*}/secure-filesystem.sh"
+source "${BASH_SOURCE%/*}/build/secure-filesystem.sh"
