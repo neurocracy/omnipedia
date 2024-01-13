403960e45d0e47337786a083a4cfb7fc56a7c6cc
Matei Stanca <i@ambientimpact.com>
2024-01-13 10:57:55 -0500

    .do build-*.sh: Moved Bash relative script @see to header docblock.

--- .do/build-background-tasks.sh
+++ .do/build-background-tasks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source "${BASH_SOURCE%/*}/build/composer.sh"
-
-# Secure the file system by removing write permissions.
+# Build the background tasks worker.
 #
 # @see https://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script#6659698
+#   Describes how to include a Bash script relative to the current path.
+
+source "${BASH_SOURCE%/*}/build/composer.sh"
+
 source "${BASH_SOURCE%/*}/build/secure-filesystem.sh"
--- .do/build-web.sh
+++ .do/build-web.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Build the web service.
+#
+# @see https://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script#6659698
+#   Describes how to include a Bash script relative to the current path.
 
 source "${BASH_SOURCE%/*}/build/composer.sh"
 
 source "${BASH_SOURCE%/*}/build/yarn.sh"
 
-# @see https://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script#6659698
 source "${BASH_SOURCE%/*}/build/secure-filesystem.sh"
