f81f1663e6c9f3e9de8b3e0e08670f67bd98f0d8
Matei Stanca <i@ambientimpact.com>
2024-02-26 19:02:26 -0500

    Reworked hard-coded .do/build/yarn.sh to read package.json w/ jq:
    This allows us to keep the workspace dependencies where they make more
    sense and makes the yarn.sh reusable and automagic.

--- .do/build/yarn.sh
+++ .do/build/yarn.sh
@@ -5,16 +5,15 @@
 # buildpack always runs after the Node.js buildpack, we have to add these
 # workspace items after the fact because they're only present after Composer has
 # pulled them in and would otherwise stop the build with an error when Yarn
-# would try to find them.
-yarn add \
-  "drupal-ambientimpact-core@workspace:^2" \
-  "drupal-ambientimpact-icon@workspace:^1" \
-  "drupal-ambientimpact-media@workspace:^4" \
-  "drupal-ambientimpact-ux@workspace:^1" \
-  "drupal-omnipedia-block@workspace:^6" \
-  "drupal-omnipedia-changes@workspace:^7" \
-  "drupal-omnipedia-content@workspace:^6" \
-  "drupal-omnipedia-media@workspace:^6" \
-  "drupal-omnipedia-site-theme@workspace:^7"
+# would try to find them. To accomplish this, they're instead listed in a
+# "doDependencies" key and not under "dependencies" or "devDependencies", and
+# then during the build phase we parse that using jq, format it into the syntax
+# for yarn add, and use command substitution to provide it all to Yarn.
+#
+# @see https://jqlang.github.io/jq/
+#
+# @see Aptfile
+#   Tells App Platform to install jq for us.
+yarn add $(jq '.doDependencies | to_entries[] | [.key, .value] | [join("@")] | join(" ")' package.json)
 
 yarn build:deploy
