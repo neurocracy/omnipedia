5eb9b90956d07a28085bd17d71dda8bc0c9a3d67
Matei Stanca <i@ambientimpact.com>
2024-03-01 07:51:38 -0500

    .do/build/yarn.sh: "doDependencies" to "composerDependencies":
    This makes it clearer where those dependencies are coming from rather
    than the specific platform we're dealing with.

--- .do/build/yarn.sh
+++ .do/build/yarn.sh
@@ -6,14 +6,14 @@
 # workspace items after the fact because they're only present after Composer has
 # pulled them in and would otherwise stop the build with an error when Yarn
 # would try to find them. To accomplish this, they're instead listed in a
-# "doDependencies" key and not under "dependencies" or "devDependencies", and
-# then during the build phase we parse that using jq, format it into the syntax
-# for yarn add, and use command substitution to provide it all to Yarn.
+# "composerDependencies" key and not under "dependencies" or "devDependencies",
+# and then during the build phase we parse that using jq, format it into the
+# syntax for yarn add, and use command substitution to provide it all to Yarn.
 #
 # @see https://jqlang.github.io/jq/
 #
 # @see Aptfile
 #   Tells App Platform to install jq for us.
-yarn add $(jq --raw-output '.doDependencies | to_entries[] | [.key, .value] | [join("@")] | join(" ")' package.json)
+yarn add $(jq --raw-output '.composerDependencies | to_entries[] | [.key, .value] | [join("@")] | join(" ")' package.json)
 
 yarn build:deploy
