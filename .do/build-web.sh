f6e930cc0ccfa89b1bfe168695a3a4ebb4f4ddee
Matei Stanca <i@ambientimpact.com>
2024-05-29 19:30:02 -0400

    Issue #49: Added explanation of explicit exit in .do/build-*.sh scripts.

--- .do/build-web.sh
+++ .do/build-web.sh
@@ -6,4 +6,7 @@
 .do/build/yarn.sh && \
 .do/build/secure-filesystem.sh
 
+# Explicitly exit here using the most recent exit code from the above. Note the
+# need to chain them all using the && operator to ensure an error in one stops
+# execution immediately.
 exit $?
