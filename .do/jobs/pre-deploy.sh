c75967fb1c32e4c87e94840afd881f17eba33e71
Matei Stanca <i@ambientimpact.com>
2025-07-14 08:35:54 -0400

    Moved pre-deploy deployment active state:set to start of script.

--- .do/jobs/pre-deploy.sh
+++ .do/jobs/pre-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+drush state:set digitalocean.deployment_active true
+
 # Enable maintenance mode.
 # drush maint:set 1
 # echo "=> Maintenance mode has been turned on"
@@ -9,5 +11,3 @@ drush -y updb
 
 # Import any configuration changes. This should be run after database updates.
 drush -y config:import
-
-drush state:set digitalocean.deployment_active true
